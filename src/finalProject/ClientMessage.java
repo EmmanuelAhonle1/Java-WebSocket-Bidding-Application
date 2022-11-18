ac21d81926f80b24eae67aa8fc026d910693068d
mobuc <mobuc@MSI>
2022-11-18 00:39:44 -0600

    Initial setup

---
+++ src/finalProject/ClientMessage.java
@@ -0,0 +1,21 @@
+package finalProject;
+
+class ClientMessage {
+  String type;
+  String input;
+  int number;
+
+  protected ClientMessage() {
+    this.type = "";
+    this.input = "";
+    this.number = 0;
+    System.out.println("client-side message created");
+  }
+
+  protected ClientMessage(String type, String input, int number) {
+    this.type = type;
+    this.input = input;
+    this.number = number;
+    System.out.println("client-side message created");
+  }
+}
